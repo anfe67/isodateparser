9c003f2f5b8afc938533050aeb0dcb8d4e3ba362
Pieter Provoost <pieterprovoost@gmail.com>
2020-10-21 12:25:48 +0200

    refactoring ongoing

--- isodateparser/__init__.py
+++ isodateparser/__init__.py
@@ -3,11 +3,33 @@ import logging
 import json
 from calendar import monthrange
 import datetime
+from enum import Enum, auto
 
 class Token(object):
     def __init__(self, kind, value=None):
         self.kind = kind
         self.value = value
+
+class States(Enum):
+    BEGIN = auto()
+    YEAR = auto()
+    YEAR_SEP = auto()
+    MONTH = auto()
+    MONTH_SEP = auto()
+    DAY = auto()
+
+class Tokens(Enum):
+    NUMBER = auto()
+    DATETIMESEPARATOR = auto()
+    TIMESEPARATOR = auto()
+    INTERVALSEPARATOR = auto()
+    UTC = auto()
+    TIMEZONESIGN = auto()
+    MINUS = auto()
+    PERIOD = auto()
+    DESIGNATOR = auto()
+    REPEAT = auto()
+    DATESEPARATOR = auto()
 
 class ISODateParser(object):
 
@@ -69,80 +91,32 @@ class ISODateParser(object):
         self._logger.debug(self._print_tokens(self._tokens))
 
     def _parse(self):
-        pass
+        part = "start"
+        state = States.BEGIN
 
-    # def _parse(self):
-    #     state_primary = "start"
-    #     state_secondary = "datetime"
-    #     state_time = False
-    #     previous_token = None
-    #     for token in self._tokens:
-    #         if token.kind == "DATESEPARATOR":
-    #             if state_secondary not in "datetime":
-    #                 raise ValueError("Unexpected token " + token.value)
-    #             if previous_token is None or previous_token.kind != "NUMBER":
-    #                 raise ValueError("Unexpected token " + token.value)
-    #         elif token.kind == "TIMESEPARATOR":
-    #             if state_secondary not in ("datetime", "timezone"):
-    #                 raise ValueError("Unexpected token " + token.value)
-    #             if previous_token is None or previous_token.kind != "NUMBER":
-    #                 raise ValueError("Unexpected token " + token.value)
-    #         elif token.kind == "DATETIMESEPARATOR":
-    #             if state_secondary not in "datetime":
-    #                 raise ValueError("Unexpected token " + token.value)
-    #             if previous_token is None or previous_token.kind != "NUMBER":
-    #                 raise ValueError("Unexpected token " + token.value)
-    #         elif token.kind == "TIMEZONESIGN":
-    #             if not state_time:
-    #                 raise ValueError("Unexpected token " + token.value)
-    #         if token.kind == "DATETIMESEPARATOR":
-    #             state_time = True
-    #         elif token.kind == "INTERVALSEPARATOR":
-    #             state_primary = "end"
-    #             state_secondary = "datetime"
-    #             state_time = False
-    #         elif token.kind == "PERIOD":
-    #             state_secondary = "duration"
-    #         elif token.kind == "TIMEZONESIGN":
-    #             state_secondary = "timezone"
-    #         elif token.kind == "UTC":
-    #             state_secondary = "timezone"
-    #         if token.kind in ("NUMBER", "DESIGNATOR", "TIMEZONESIGN", "UTC"):
-    #             self._parsed[state_primary][state_secondary].append(token)
-    #         previous_token = token
-    #     self._shift(self._parsed)
+        for token in self._tokens:
 
-    # def _populate(self):
-    #     parts = ["year", "month", "day", "hours", "minutes", "seconds", "microseconds"]
-    #     for i in ["start", "end"]:
-    #         for j in range(len(self._parsed[i]["datetime"])):
-    #             if self._parsed[i]["datetime"][j]:
-    #                 value = self._parsed[i]["datetime"][j].value
-    #             elif i == "end":
-    #                 value = self._parsed["start"]["datetime"][j].value
-    #             self.components[i][parts[j]] = int(value)
-    #     for i in ["start", "end"]:
-    #         state = 0
-    #         hours = None
-    #         minutes = 0.0
-    #         sign = 1.0
-    #         for token in self._parsed[i]["timezone"]:
-    #             if token.kind == "UTC":
-    #                 self.components[i]["timezone"] = 0.0
-    #             elif token.kind == "TIMEZONESIGN" and token.value == "-":
-    #                 sign = -1.0
-    #             elif token.kind == "NUMBER":
-    #                 if len(token.value) == 4:
-    #                     hours = int(token.value[0:2])
-    #                     minutes = int(token.value[2:4])
-    #                 if len(token.value) == 2:
-    #                     if state == 0:
-    #                         hours = int(token.value)
-    #                         state += 1
-    #                     elif state == 1:
-    #                         minutes = int(token.value)
-    #         if hours is not None:
-    #             self.components[i]["timezone"] = sign * (hours + minutes / 60.0)
+            if state == States.BEGIN:
+                if token.kind == Tokens.NUMBER:
+                    self.components[part]["year"] = token.value
+                    state = States.YEAR
+                    continue
+                raise ValueError("Unexpected token: " + token.value)
+
+            elif state == States.YEAR:
+                if token.kind == Tokens.DATESEPARATOR:
+                    state = States.YEAR_SEP
+                    continue
+                raise ValueError("Unexpected token: " + token.value)
+
+            elif state == States.YEAR_SEP:
+                if token.kind == Tokens.NUMBER:
+                    self.components[part]["month"] = token.value
+                    state = States.MONTH
+                    continue
+                raise ValueError("Unexpected token: " + token.value)
+
+
 
     # def _make_datetimes(self):
     #     for i in ("start", "end"):
@@ -191,27 +165,27 @@ class ISODateParser(object):
                 buffer.append(c)
             else:
                 if len(buffer) > 0:
-                    self._tokens.append(Token("NUMBER", "".join(buffer)))
+                    self._tokens.append(Token(Tokens.NUMBER, "".join(buffer)))
                     # todo: number of length 8 should be split into 4-2-2
                     buffer = list()
                 if c == " " or c == "T":
-                    self._tokens.append(Token("DATETIMESEPARATOR", c))
+                    self._tokens.append(Token(Tokens.DATETIMESEPARATOR, c))
                 elif c == ":":
-                    self._tokens.append(Token("TIMESEPARATOR", c))
+                    self._tokens.append(Token(Tokens.TIMESEPARATOR, c))
                 elif c == "/":
-                    self._tokens.append(Token("INTERVALSEPARATOR", c))
+                    self._tokens.append(Token(Tokens.INTERVALSEPARATOR, c))
                 elif c == "Z":
-                    self._tokens.append(Token("UTC", c))
+                    self._tokens.append(Token(Tokens.UTC, c))
                 elif c == "+":
-                    self._tokens.append(Token("TIMEZONESIGN", c))
+                    self._tokens.append(Token(Tokens.TIMEZONESIGN, c))
                 elif c == "-" or c == "−":
-                    self._tokens.append(Token("MINUS", c))
+                    self._tokens.append(Token(Tokens.MINUS, c))
                 elif c == "P":
-                    self._tokens.append(Token("PERIOD", c))
+                    self._tokens.append(Token(Tokens.PERIOD, c))
                 elif c in ["Y", "M", "W", "D", "H", "M", "S"]:
-                    self._tokens.append(Token("DESIGNATOR", c))
+                    self._tokens.append(Token(Tokens.DESIGNATOR, c))
                 elif c == "R":
-                    self._tokens.append(Token("REPEAT", c))
+                    self._tokens.append(Token(Tokens.REPEAT, c))
                 elif c == "\n":
                     pass
                 else:
@@ -230,20 +204,20 @@ class ISODateParser(object):
     def _disambiguate(self):
         time = False
         for i, token in enumerate(self._tokens):
-            if token.kind == "TIMESEPARATOR":
+            if token.kind == Tokens.TIMESEPARATOR:
                 time = True
-            elif token.kind == "INTERVALSEPARATOR":
+            elif token.kind == Tokens.INTERVALSEPARATOR:
                 time = False
-            elif token.kind == "MINUS":
+            elif token.kind == Tokens.MINUS:
                 if time:
-                    self._tokens[i] = Token("TIMEZONESIGN", "-")
+                    self._tokens[i] = Token(Tokens.TIMEZONESIGN, "-")
                 else:
-                    self._tokens[i] = Token("DATESEPARATOR", "-")
+                    self._tokens[i] = Token(Tokens.DATESEPARATOR, "-")
 
     def _print_tokens(self, tokens):
         lines = list()
         for token in tokens:
-            line = token.kind
+            line = str(token.kind)
             if token.value is not None:
                 line += " (" + str(token.value) + ")"
             lines.append(line)
